d4904e96004e8b6fafd1df9a6767f68e449ab117
noErrdev <dbongo0701@gmail.com>
2023-01-02 22:40:17 +0100

    Feat: Upgraded auth templates and validation

--- core/views.py
+++ core/views.py
@@ -18,10 +18,12 @@ def sign_up(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
+            email = form.cleaned_data['email'].lower()
         # Create a new user object but avoid saving it yet
             user = form.save(commit=False)
-        # Set the chosen password
-            user.set_password(form.cleaned_data['password'])
+        # Set the chosen password and email
+            user.email = email
+            user.set_password(form.cleaned_data['password1'])
         # Save the User object
             user.save()
             return render(request,'signup_done.html',{'user': user})
