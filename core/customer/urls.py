74834a11fcd9a47da7f95ffe69381bcde1db9966
noErrdev <dbongo0701@gmail.com>
2023-01-13 01:56:30 +0100

    Implemented custom form for customer

--- core/customer/urls.py
+++ core/customer/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path 
-from .views import customer_index
+from .views import customer_index, customer_profile
 
 app_name = "customer"
 
 urlpatterns =  [
     path("", customer_index, name="home"),
+    path("profile/", customer_profile, name="profile")
 ]
--- core/customer/views.py
+++ core/customer/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from .forms import CustomerProfileForm
 
 @login_required
 def customer_index(request):
-    return render(request, "customer/base.html")
+    return redirect(reverse('customer:profile'))
+
+@login_required(login_url="/sign-in/?next=/customer/")
+def customer_profile(request):
+    form = CustomerProfileForm(instance=request.user)
+
+    if request.method == "POST":
+        form = CustomerProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('customer:profile'))
+    return render(request, "customer/profile.html", { "form": form })
