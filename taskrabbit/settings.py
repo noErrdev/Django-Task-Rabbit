0f916d8b5138d4b4d9ae391bd313ad04c5485819
noErrdev <dbongo0701@gmail.com>
2023-03-02 18:59:14 +0100

    commit: added middleware for customer model

--- taskrabbit/settings.py
+++ taskrabbit/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "core.middleware.ProfileMiddleware",
 ]
 
 ROOT_URLCONF = "taskrabbit.urls"
