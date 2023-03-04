baa8c4012524cbfb30552f474364a649ac47a636
noErrdev <dbongo0701@gmail.com>
2023-03-04 17:47:51 +0100

    commit: created a form for the creation of jobs

--- core/customer/forms.py
+++ core/customer/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 
-from core.models import Customer
+from core.models import Customer, Job
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
@@ -12,3 +12,8 @@ class BasicCustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = ("avatar",)
+
+class JobCreationForm(forms.ModelForm):
+    class Meta:
+        model = Job
+        fields = ["name", "description", "category", "size", "quantity", "photo"]
