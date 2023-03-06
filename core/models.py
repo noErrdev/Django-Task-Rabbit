ca5d5f555210a20233285b125ed4662e2a42fa51
noErrdev <dbongo0701@gmail.com>
2023-03-06 22:17:10 +0100

    commit: added props to the job model for payment

--- core/models.py
+++ core/models.py
@@ -72,6 +72,10 @@ class Job(models.Model):
     delivery_name = models.CharField(max_length=300, blank=True)
     delivery_phone_number = models.CharField(max_length=50, blank=True)
 
+    duration = models.IntegerField(default=0)
+    distance = models.FloatField(default=0)
+    price = models.FloatField(default=0)
+
     def __str__(self):
         return self.description
 
