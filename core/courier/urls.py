992689725ae7a447cd956d68ab4956b3e6e70a80
noErrdev <dbongo0701@gmail.com>
2023-03-08 20:24:13 +0100

    commit: added my jobs page for courier

--- core/courier/urls.py
+++ core/courier/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path 
-from .views import courier_index, courier_available_jobs, courier_available_job
+from .views import courier_index, courier_available_jobs, courier_available_job, courier_current_job
 from .apis import courier_available_jobs_api
 
 app_name = "courier"
@@ -7,6 +7,7 @@ app_name = "courier"
 urlpatterns =  [
     path("", courier_index, name="home"),
     path("jobs/", courier_available_jobs, name="available_jobs"),
+    path("jobs/current/", courier_current_job, name="current_job"),
     path("jobs/<str:pk>/", courier_available_job, name="available_job_details"),
     path("api/jobs/", courier_available_jobs_api, name="available_jobs_api"),
 ]
--- core/courier/views.py
+++ core/courier/views.py
@@ -22,13 +22,24 @@ def courier_available_job(request, pk):
     job = Job.objects.filter(id=pk, status=Job.PROCESSING).last()
 
     if not job:
-        return redirect(reverse('courier:available_jobs'))
+        return redirect(reverse(available_jobs_namespace))
     if request.method == "POST":
         job.courier = request.user.courier
         job.status = Job.PICKING
         job.save()
-        return redirect(reverse('courier:available_jobs'))
+        return redirect(reverse(available_jobs_namespace))
     return render(request, "courier/available-job-details.html", { "job": job } )
+
+@login_required(login_url="/sign-in/?next=/courier/")
+def courier_current_job(request):
+    job = Job.objects.filter(
+        courier=request.user.courier, 
+        status__in=[
+            Job.PICKING,
+            Job.DELIVERING
+        ]
+    ).last()
+    return render(request, "courier/current-job.html", { "job": job })
 
 
 
