7cdcb944f12d6933e0f14c03f41023489809beb6
noErrdev <dbongo0701@gmail.com>
2023-03-08 14:04:42 +0100

    commit: added full screen overlay for courier maps

--- core/courier/urls.py
+++ core/courier/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path 
-from .views import courier_index
+from .views import courier_index, courier_available_jobs
 
 app_name = "courier"
 
 urlpatterns =  [
     path("", courier_index, name="home"),
+    path("jobs/", courier_available_jobs, name="available_jobs"),
 ]
--- core/courier/views.py
+++ core/courier/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
-@login_required
+available_jobs_namespace = 'courier:available_jobs'
+
+@login_required(login_url="/sign-in/?next=/courier/")
 def courier_index(request):
-    return render(request, "courier.html")
+    return redirect(reverse(available_jobs_namespace))
+
+@login_required(login_url="/sign-in/?next=/courier/")
+def courier_available_jobs(request):
+    return render(request, "courier/available-jobs.html")
+
